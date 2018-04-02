683d45bdb41aaaf1a5317cb850c8af8f48a7ecc6
Zach Denton <zjd@google.com>
2018-04-02 11:58:31 -0700

    Sync font config with dwm.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "terminus:pixelsize=12:antialias=false:autohint=false";
+static char *font = "terminus:pixelsize=12";
 static int borderpx = 2;
 
 /*
