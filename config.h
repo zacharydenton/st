41549efcb8c7612ac4e7891506d9f35b4405e49b
Zach Denton <z@chdenton.com>
2018-12-08 17:19:20 -0800

    Add zenburn colorscheme.

--- config.h
+++ config.h
@@ -86,28 +86,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3f3f3f",
-	"#cc9393",
+	"#313633",
+	"#dca3a3",
 	"#7f9f7f",
-	"#d0bf8f",
-	"#6ca0a3",
-	"#dc8cc3",
-	"#93e0e3",
-	"#dcdccc",
+	"#e3ceab",
+	"#c0bed1",
+	"#bca3a3",
+	"#9fafaf",
+	"#f0dfaf",
 
 	/* 8 bright colors */
-	"#000000",
-	"#dca3a3",
-	"#bfebbf",
-	"#f0dfaf",
+	"#2e3330",
+	"#ffcfaf",
+	"#dfdfbf",
+	"#efdcbc",
+	"#8c8cbc",
+	"#cc9393",
 	"#8cd0d3",
-	"#dc8cc3",
-	"#93e0e3",
-	"#ffffff",
+	"#efef8f",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
+	"#dcdccc",
+	"#3f3f3f",
+	"#8faf9f",
+	"#304a3d",
 };
 
 
@@ -115,10 +119,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 7;
-static unsigned int defaultrcs = 0;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
